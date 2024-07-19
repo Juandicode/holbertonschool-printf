b7f5b5da89d077ceedc033b3283738d31e2a27b6
juandicode <juandiegomhs@gmail.com>
2024-07-19 21:18:12 +0000

    2

--- _printchar.c
+++ _printchar.c
@@ -7,46 +7,48 @@
  * @format: puntero al string
  * Return: Contador
  */
-int _printf(const char *format, ...)
+int _printf(const char *format,...)
 {
-	int contador = 0;
-	int i;
-	va_list args;
-	char *s;
-	char c;
+    int contador = 0;
+    va_list args;
+    char c;
+    char *s;
 
-	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-	if (format[i] == '%')
-	{
-	if (format[i + 1] != '\0')
-	{
-	i += 1;
-	if (format[i] == 'c')
-	{
-	c = va_arg(args, int);
-	write(1, &c, 1);
-	contador++;
-	}
-	else if (format[i] == '%')
-	{
-	write(1, "%", 1);
-	contador++;
-	}
-	else if (format[i] == 's')
-	{
-	s = va_arg(args, char *);
-	contador = contador + write(1, s, _strlen(s));
-	}
-	}
-	}
-	else
-	{
-	write(1, &format[i], 1);
-	contador++;
-	}
-	}
-	va_end(args);
-	return (contador);
+    if (*format == '\0') {
+        return 0; /* or some other default value */
+    }
+    va_start(args, format);
+    for (; *format; format++) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    contador++;
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    contador++;
+                    break;
+                case 's':
+                    s = va_arg(args, char *);
+                    if (s == NULL) {
+                        contador += write(1, "(null)", 6);
+                    } else {
+                        contador += write(1, s, _strlen(s));
+                    }
+                    break;
+                default:
+                    write(1, format - 1, 1);
+                    contador++;
+                    break;
+            }
+        } else {
+            write(1, format, 1);
+            contador++;
+        }
+    }
+    va_end(args);
+    return contador;
 }
