c869973f54a7e8dc68a3c8b3dbb857bdd8a5add4
juandicode <juandiegomhs@gmail.com>
2024-07-19 21:32:21 +0000

    2

--- _printchar.c
+++ _printchar.c
@@ -1,48 +1,48 @@
+#include <stdio.h>
+#include <unistd.h> /* para la función write*/
 #include "main.h"
-#include <unistd.h>
 #include <stdarg.h>
 
 /**
- * _printf - prints output according to format
- * @format: format string
- * Return: number of characters printed
+ * _printf - imprime caracteres, strings, y %
+ * @format: puntero a un string
+ * Return: contador
  */
 int _printf(const char *format, ...)
 {
-    int contador = 0;
+    int contador = 0, i, j;
     va_list args;
-    char c;
-    char *s;
+    char c, *s;
 
     va_start(args, format);
-    for (; *format; format++) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    c = va_arg(args, int);
-                    write(1, &c, 1);
-                    contador++;
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    contador++;
-                    break;
-                case 's':
-                    s = va_arg(args, char *);
-                    if (s == NULL) {
-                        contador += write(1, "(null)", 6);
-                    } else {
-                        contador += write(1, s, _strlen(s));
-                    }
-                    break;
-                default:
-                    write(1, "%", 1);
-                    contador++;
-                    break;
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                c = va_arg(args, int);
+                write(1, &c, 1);
+                contador++;
             }
-        } else {
-            write(1, format, 1);
+            else if (format[i] == 's')
+            {
+                s = va_arg(args, char *);
+                if (s == NULL)
+                    s = "(null)";
+                for (j = 0; s[j]; j++, contador++)
+                    write(1, &s[j], 1);
+            }
+            else if (format[i] == '%')
+            {
+                write(1, "%", 1);
+                contador++;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
             contador++;
         }
     }
