c66d4380afcc7213936fcde700ff224b81068c7c
juandicode <juandiegomhs@gmail.com>
2024-11-29 18:38:15 +0000

    corregir todo el fckng betty

--- main.h
+++ main.h
@@ -1,4 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _printf(const char *format, ...);
 void printch(char c, int *contador);
 int printstr(char *s, int *contador);
 void printpercent(int *contador);
+#endif
